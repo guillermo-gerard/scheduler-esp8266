239ded5307ef5431da5c5562145de085c3b6b0c0
guillermo_gerard <guilloag@gmail.com>
2022-06-10 21:59:36 -0300

    ajax call  added to get the hour

--- src/main.cpp
+++ src/main.cpp
@@ -11,96 +11,119 @@
 #include <WifiConfigPersistor.h>
 #include <DNSServer.h>
 #include <ESP8266mDNS.h>
+#include <NTPClient.h>
 
 void SetupCaptivePortalMode(const char *portalName);
 void ConnectToWifi();
 void ConfigureServerEndpoints();
 
-const char *filename = "/config.txt";
+const char *filename = "/config.json";
 WifiConfigPersistor wifiConfigPersistor(filename);
 AsyncWebServer server(80);
 DNSServer dnsServer;
 
-const uint8_t portalButton = D1; //TODO: confirm the location and logic of this button with Sebastian
+const uint8_t portalButton = D1; // TODO: confirm the location and logic of this button with Sebastian
 
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP);
 
+void setup()
+{
+    Serial.begin(115200);
 
-void setup(){
-Serial.begin(115200);
-
-  if (!LittleFS.begin())
-  {
-    Serial.println(F("An Error has occurred while mounting LittleFS"));
-    return;
-  }
-
-  //The captive portal will be set only if the button is pressed when booting
-  //after the config file is saved, the esp will be restarted automatically
-  if (digitalRead(portalButton))
-  {
-    SetupCaptivePortalMode("scheduler-config");
-    while (true)
+    if (!LittleFS.begin())
     {
-      //The only way to get out of this will be a manual reset OR to fill in the credentials (it will reboot automatically)
-      yield();
-      dnsServer.processNextRequest();
+        Serial.println(F("An Error has occurred while mounting LittleFS"));
+        return;
     }
-  }
-  ConnectToWifi();
+
+    // The captive portal will be set only if the button is pressed when booting
+    // after the config file is saved, the esp will be restarted automatically
+    if (digitalRead(portalButton))
+    {
+        SetupCaptivePortalMode("scheduler-config");
+        while (true)
+        {
+            // The only way to get out of this will be a manual reset OR to fill in the credentials (it will reboot automatically)
+            yield();
+            dnsServer.processNextRequest();
+        }
+    }
+    ConnectToWifi();
+    timeClient.setTimeOffset(-10800);
 }
 
-void loop(){
+void loop()
+{
 }
-
 
 void ConnectToWifi()
 {
-  WiFi.mode(WIFI_STA);
-  Config wifiConfig = wifiConfigPersistor.Retrieve();
-  WiFi.begin(wifiConfig.ssid, wifiConfig.pass);
-  Serial.println("");
-  Serial.print("trying to connect to Wifi");
- 
-  if (WiFi.waitForConnectResult(30000) != WL_CONNECTED)
-  {
-    Serial.println("WiFi connection failed!");
-    delay(2000);
-  }
-  else
-  {
-    Serial.println("WiFi connection succeed!");
-    ConfigureServerEndpoints();
-    server.begin();
-    Serial.print("IP Address: ");
-    Serial.println(WiFi.localIP());
-    if (!MDNS.begin("scheduler")) {
-        Serial.println("Error setting up MDNS responder!");
+    WiFi.mode(WIFI_STA);
+    Config wifiConfig = wifiConfigPersistor.Retrieve();
+    WiFi.begin(wifiConfig.ssid, wifiConfig.pass);
+    Serial.println("");
+    Serial.print("trying to connect to Wifi");
+
+    if (WiFi.waitForConnectResult(30000) != WL_CONNECTED)
+    {
+        Serial.println("WiFi connection failed!");
+        delay(2000);
     }
-    
-    delay(2000);
-  }
+    else
+    {
+        Serial.println("WiFi connection succeed!");
+        ConfigureServerEndpoints();
+        server.begin();
+        Serial.print("IP Address: ");
+        Serial.println(WiFi.localIP());
+        if (!MDNS.begin("esp8266"))
+        {
+            Serial.println("Error setting up MDNS responder!");
+        }
+        timeClient.begin();
+        delay(2000);
+    }
+}
+
+String processor(const String &var)
+{
+    if (var == "HORA")
+    {
+        timeClient.update();
+        return timeClient.getFormattedTime();
+    }
+    if (var == "TEMPERATURA")
+    {
+        return String(9.0);
+    }
+    return String();
 }
 
 void ConfigureServerEndpoints()
 {
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
-            {
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+              {
               Serial.println(request->url());
-              request->send(LittleFS, "/dashboard.html", String(), false);
-            });
+              request->send(LittleFS, "/dashboard.html", String(), false, processor); });
 
-  server.on("/bootstrap.css", HTTP_GET, [](AsyncWebServerRequest *request)
-            {
+    server.on("/ajax_info_hora", HTTP_GET, [](AsyncWebServerRequest *request)
+              {
               Serial.println(request->url());
-              request->send(LittleFS, "/bootstrap.css", "text/css");
-            });
+              timeClient.update();
+         
+              request->send(200, "text/plain", timeClient.getFormattedTime()); });
+
+    server.on("/bootstrap.css", HTTP_GET, [](AsyncWebServerRequest *request)
+              {
+              Serial.println(request->url());
+              request->send(LittleFS, "/bootstrap.css", "text/css"); });
 }
 
 void SetupCaptivePortalMode(const char *portalName)
 {
-  WiFi.softAP(portalName);
-  dnsServer.start(53, "*", WiFi.softAPIP());
-  server.addHandler(new CaptiveRequestHandler(&wifiConfigPersistor)).setFilter(ON_AP_FILTER); //only when requested from AP
-  server.begin();
+    WiFi.softAP(portalName);
+    dnsServer.start(53, "*", WiFi.softAPIP());
+    server.addHandler(new CaptiveRequestHandler(&wifiConfigPersistor)).setFilter(ON_AP_FILTER); // only when requested from AP
+    server.begin();
 }
-
